d38fef7f7df68fc6ef0f42950216fea2cf4d1171
ikki-techy <pathanfarhaan87@gmail.com>
2025-04-19 00:17:42 +0530

    Defines User and Post tables.

--- echopost_flask/models.py
+++ echopost_flask/models.py
@@ -13,3 +13,12 @@ class User(db.Model):
         return f"User('{self.username}','{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.Datetime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
