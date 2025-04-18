1344d1e14f78d13c3b18cefed4c36c9fdb914566
ikki-techy <pathanfarhaan87@gmail.com>
2025-04-18 23:32:49 +0530

    Initial project upload

---
+++ echopost_flask/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from echopost_flask.config import Config
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    from echopost_flask.users.routes import users
+    from echopost_flask.posts.routes import posts
+    from echopost_flask.main.routes import main
+    from echopost_flask.errors.handlers import errors
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
+
---
+++ echopost_flask/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'f1b3cf34b2e5a8d2c54ad'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
